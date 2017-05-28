2b69bcd229a9908c2702c2209e5f18db0514ae45
Alex <mikheevoleksandr@gmail.com>
2017-05-28 15:47:18 +0300

    add new_number function which generates new 2 in empty field after each move

--- change_state.py
+++ change_state.py
@@ -2,11 +2,13 @@ from flask import Flask, request, jsonify
 import copy
 import sys
 import numpy as np
+import random
 
 app = Flask(__name__)
 
 
 def left(a):
+    check = copy.deepcopy(a)
     for i in range(4):
         for j in range(3):
             if a[i][j] is None:
@@ -15,9 +17,12 @@ def left(a):
             elif a[i][j] == a[i][j + 1]:
                 a[i][j] *= 2
                 a[i][j + 1] = None
+    if check != a:
+        new_number(a)
 
 
 def right(a):
+    check = copy.deepcopy(a)
     for i in range(4):
         for j in range(3, 0, -1):
             if a[i][j] is None:
@@ -26,9 +31,12 @@ def right(a):
             elif a[i][j] == a[i][j - 1]:
                 a[i][j] *= 2
                 a[i][j - 1] = None
+    if check != a:
+        new_number(a)
 
 
 def up(a):
+    check = copy.deepcopy(a)
     for j in range(4):
         for i in range(3):
             if a[i][j] is None:
@@ -37,9 +45,12 @@ def up(a):
             elif a[i][j] == a[i + 1][j]:
                 a[i][j] *= 2
                 a[i + 1][j] = None
+    if check != a:
+        new_number(a)
 
 
 def down(a):
+    check = copy.deepcopy(a)
     for j in range(4):
         for i in range(3, 0, -1):
             if a[i][j] is None:
@@ -48,17 +59,35 @@ def down(a):
             elif a[i][j] == a[i - 1][j]:
                 a[i][j] *= 2
                 a[i - 1][j] = None
+    if check != a:
+        new_number(a)
 
 
 def restart(a):
     a = copy.deepcopy(start)
     return a
 
+
+def new_number(a):
+    options = [[i, j] for i in range(4) for j in range(4) if a[i][j] is None]
+    i, j = random.choice(options)
+    a[i][j] = 2
+
 start = [[4, 4, 4, 4],
          [None, 8, None, 8],
          [None, 8, 8, None],
          [None, None, None, None]]
 field = copy.deepcopy(start)
+
+
+def show(a):
+    for i in range(4):
+        for j in range(4):
+            if a[i][j] is None:
+                print(a[i][j], end=" ")
+            else:
+                print(a[i][j], end="\t")
+        print()
 
 
 @app.route('/', methods=["POST"])
@@ -78,7 +107,7 @@ def change_state():
     elif action == "right":
         right(field)
 
-    #show(field)
+    show(field)
     out_field = np.array(field)
     out_field.resize(16)
     out_field = jsonify({"field": list(out_field)})
